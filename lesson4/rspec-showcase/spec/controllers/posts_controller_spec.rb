24c087b94cbe480c6cbb7fe0c1b93b6720e74318
railkun <111naruto111@gmail.com>
2016-11-24 14:23:55 +0200

    correction http methods put

--- lesson4/rspec-showcase/spec/controllers/posts_controller_spec.rb
+++ lesson4/rspec-showcase/spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe 'GET #publish' do
+  describe 'PUT #publish' do
     it 'changes published to true' do
       post = Post.create!(id: 1, body: 'blabla', title: 'test', user_id: 1, published: false)
       put :publish, id: post.id
@@ -10,7 +10,7 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe 'GET #unpublish' do
+  describe 'PUT #unpublish' do
     it 'changes published to false' do
       post = Post.create!(id: 1, body: 'blabla', title: 'test', user_id: 1, published: true)
       put :unpublish, id: post.id
